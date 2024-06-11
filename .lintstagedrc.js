ea745818bd9331052c18023440482009d9c0bd7e
Ahmedtahoon2 <tahooooon3000@gmail.com>
2024-06-11 17:44:18 +0300

    feat: fixing

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,5 +6,5 @@ const buildEslintCommand = filenames =>
     .join(" --file ")}`;
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": ["npm run format", buildEslintCommand],
+  "*.{js,jsx,ts,tsx}": ["prettier --write", buildEslintCommand],
 };
